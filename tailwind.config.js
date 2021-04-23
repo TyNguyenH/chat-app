8ccb0a27a34e4f7950a56ebc05f359ecc8fe205a
TyNguyenH <NguyenHuuTy189@gmail.com>
2021-04-23 22:15:51 +0700

    Add tailwindcss and postcss config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    purge: [
+        './public/*.html',
+        './public/*.ejs',
+        './public/partials/*.ejs',
+        './public/js/*.js'
+    ],
+    theme: {},
+    variants: {},
+    plugins: [],
+}
