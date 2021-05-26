0b4875775e499bab9837c4faeb0a5775b598a67e
TyNguyenH <NguyenHuuTy189@gmail.com>
2021-05-26 14:32:51 +0700

    Add ability to update password for normal user

--- public/js/personal-info.js
+++ public/js/personal-info.js
@@ -1,26 +1,44 @@
 import CONFIG from './config.js';
 
+
+sessionStorage.clear();
+
 fetch(`${CONFIG.serverAddress}:${CONFIG.serverPort}/api/personal-info`)
     .then(response => response.json())
     .then(data => {
         let userAvatarSrc = document.querySelector('#avatar');
-        userAvatarSrc.setAttribute('src', data.avatarSrc);
+        if (userAvatarSrc) {
+            userAvatarSrc.setAttribute('src', data.avatarSrc);
+        }
 
+        let userID = document.querySelector('#user-id');
+        if (userID) {
+            userID.textContent = data.userID;
+        }
+        
         let lastNameInfo = document.querySelector('#last-name');
-        lastNameInfo.textContent = data.lastName;
-
+        if (lastNameInfo) {
+            lastNameInfo.textContent = data.lastName;
+        }
+        
         let firstNameInfo = document.querySelector('#first-name');
-        firstNameInfo.textContent = data.firstName;
+        if (firstNameInfo) {
+            firstNameInfo.textContent = data.firstName;
+        }
 
         let emailInfo = document.querySelector('#email');
-        emailInfo.textContent = data.email;
+        if (emailInfo) {
+            emailInfo.textContent = data.email;
+        }
 
         let joinDate = document.querySelector('#join-date');
-        joinDate.textContent = data.dateJoined;
+        if (joinDate) {
+            joinDate.textContent = data.dateJoined;
+        }
 
         let activeStatus = document.querySelector('#active-status');
 
-        if (data.isActive == true) {
+        if (activeStatus && data.isActive == true) {
             activeStatus.textContent = 'Đang hoạt động';
             if (document.querySelector('#active-status-btns')) {
                 let funcionButtons = document.querySelector('#active-status-btns');
@@ -51,7 +69,7 @@ fetch(`${CONFIG.serverAddress}:${CONFIG.serverPort}/api/personal-info`)
             }
         }
 
-        if (data.isActive == false) {
+        if (activeStatus && data.isActive == false) {
             activeStatus.textContent = 'Không hoạt động';
             if (document.querySelector('#active-status-btns')) {
                 let funcionButtons = document.querySelector('#active-status-btns');
@@ -81,4 +99,137 @@ fetch(`${CONFIG.serverAddress}:${CONFIG.serverPort}/api/personal-info`)
                 }
             }
         }  
-    })
+    });
+
+
+
+
+
+
+// Change password button
+let changePasswordBtn = document.querySelector('#change-password-btn');
+changePasswordBtn.onclick = () => {
+    let fullInfoWrapper = document.querySelector('#full-info-wrapper');
+    if (fullInfoWrapper) {
+        fullInfoWrapper.remove();
+        
+        // Create old password element
+        let oldPasswordElement = document.createElement('div');
+        {
+            const showOldPassword = `
+                let password = document.querySelector('#old-password');
+                if (password.type === 'password') {
+                    password.type = 'text';
+                } else {
+                    password.type = 'password';
+                }
+            `;
+
+            oldPasswordElement.innerHTML = `
+                <div class="flex flex-row items-center">
+                    <label for="old-password" class="w-1/3 text-lg font-semibold">Mật khẩu cũ:</label>
+                    <input id="old-password" type="password" 
+                        class="
+                            focus:outline-none w-2/3 px-3 py-1
+                            focus:ring-2 focus:ring-purple-600 focus:border-transparent focus:bg-gray-100
+                            rounded-lg shadow-lg bg-gray-200 break-words
+                            transition duration-150">
+                </div>
+                <div class="flex flex-row justify-end mx-1 mb-9">
+                    <div id="old-password-warning" class="mt-6 w-5/12 text-red-500 invisible">* Tối thiểu 6 ký tự</div>
+                    <div class="mt-2 mb-4 flex self-start justify-end items-center">
+                        <label class="mx-1 text-sm text-center">Hiện mật khẩu</label>
+                        <input id="show-old-password" type="checkbox" class="h-4 w-4" onclick="${showOldPassword}">
+                    </div>
+                </div>
+            `;
+        }
+
+        // Create new password element
+        let newPasswordElement = document.createElement('div');
+        {
+            const showNewPassword = `
+                let password = document.querySelector('#new-password');
+                if (password.type === 'password') {
+                    password.type = 'text';
+                } else {
+                    password.type = 'password';
+                }
+            `;
+
+            newPasswordElement.innerHTML = `
+                <div class="flex flex-row items-center">
+                    <div class="w-1/3 text-lg font-semibold">Mật khẩu mới:</div>
+                    <input id="new-password" type="password" 
+                        class="
+                            focus:outline-none w-2/3 px-3 py-1
+                            focus:ring-2 focus:ring-purple-600 focus:border-transparent focus:bg-gray-100
+                            rounded-lg shadow-lg bg-gray-200 break-words
+                            transition duration-150">
+                </div>
+                <div class="flex flex-row justify-end mx-1 mb-8">
+                    <div class="w-4/12 mt-6">
+                        <ul id="password-requirements" class="ml-1 text-red-500 visible text-base">
+                            <li class="text-black">Yêu cầu mật khẩu:</li>
+                            <li id="pass-req-min-letter">Tối thiểu 6 ký tự</li>
+                            <li id="pass-req-cap-letter">Có chữ in hoa</li>
+                            <li id="pass-req-norm-letter">Có chữ thường</li>
+                            <li id="pass-req-num-letter">Có chữ số</li>
+                        </ul>
+                    </div>
+                    <div class="w-4/12 flex mt-2 self-start justify-end items-center">
+                        <label class="mx-1 text-sm text-center">Hiện mật khẩu</label>
+                        <input id="show-new-password" type="checkbox" class="h-4 w-4" onclick="${showNewPassword}">
+                    </div>
+                </div>
+            `;
+        }
+        
+        // Create changing password element
+        let changePasswordElement = document.createElement('div');
+        changePasswordElement.innerHTML = `
+            <div id="change-password-wrapper" class="mt-10 mx-auto max-w-md">
+                <div class="mb-6 font-bold text-center text-2xl">Đổi mật khẩu</div>
+                <div class="grid grid-cols-1 mx-auto w-full">
+                    ${oldPasswordElement.innerHTML}
+                    ${newPasswordElement.innerHTML}
+                    <div class="mt-2 text-center">
+                        <button
+                            id="reset-btn"
+                            class="
+                                mx-1 px-2 py-1 rounded-lg bg-black shadow-lg
+                                font-semibold text-white text-center
+                                focus:outline-none
+                                transform hover:scale-105
+                                transition duration-75
+                            ">
+                            Nhập lại
+                        </button>
+                        <button
+                            id="update-btn"
+                            class="
+                                mx-1 px-2 py-1 rounded-lg bg-blue-500 shadow-lg
+                                font-semibold text-white text-center
+                                focus:outline-none
+                                transform hover:scale-105
+                                transition duration-75
+                            ">
+                            Cập nhật
+                        </button>
+                    </div>                
+                </div>
+            </div>
+        `;
+
+        // Remove personal-info.js
+        document.querySelector('script[src="/js/personal-info.js"]').remove();
+
+        // Append script for handling updating password
+        let updatePasswordScript = document.createElement('script');
+        updatePasswordScript.setAttribute('type', 'module');
+        updatePasswordScript.setAttribute('src', '/js/update-password.js');
+        
+        document.body.appendChild(changePasswordElement);
+        document.body.appendChild(updatePasswordScript);
+    }
+}
---
+++ public/js/update-password.js
@@ -0,0 +1,197 @@
+import CONFIG from './config.js';
+
+
+
+
+let changePasswordWrapper = document.querySelector('#change-password-wrapper');
+let oldPasswordElement = document.querySelector('#old-password');
+let newPasswordElement = document.querySelector('#new-password');
+
+function checkOldPassword() {
+    const passwordVal = oldPasswordElement.value;
+    let oldPasswordWarning = document.querySelector('#old-password-warning');
+
+    if (passwordVal.length >= 6) {
+        oldPasswordWarning.classList.add('invisible');
+        return true;
+    } else {
+        if (oldPasswordWarning.classList.contains('invisible')) {
+            oldPasswordWarning.classList.remove('invisible');
+        }
+        return false;
+    }
+}
+
+function checkNewPassword() {
+    const passwordVal = newPasswordElement.value;
+
+    let minLetters = false;
+    let capLetter = false;
+    let normalLetter = false;
+    let numericLetter = false;
+
+    if (passwordVal.length >= 6) {
+        minLetters = true;
+    }
+
+    if (passwordVal.match(/[A-Z]/g)) {
+        capLetter = true;
+    }
+
+    if (passwordVal.match(/[a-z]/g)) {
+        normalLetter = true;
+    }
+
+    if (passwordVal.match(/[0-9]/g)) {
+        numericLetter = true;
+    }
+
+    let minLettersWarn = document.querySelector('#pass-req-min-letter');
+    if (minLetters) {
+        minLettersWarn.classList.add('text-blue-500');
+    } else {
+        minLettersWarn.classList.remove('text-blue-500');
+    }
+
+    let capLettersWarn = document.querySelector('#pass-req-cap-letter');
+    if (capLetter) {
+        capLettersWarn.classList.add('text-blue-500');
+    } else {
+        capLettersWarn.classList.remove('text-blue-500');
+    }
+
+    let normalLettersWarn = document.querySelector('#pass-req-norm-letter');
+    if (normalLetter) {
+        normalLettersWarn.classList.add('text-blue-500');
+    } else {
+        normalLettersWarn.classList.remove('text-blue-500');
+    }
+
+    let numericLettersWarn = document.querySelector('#pass-req-num-letter');
+    if (numericLetter) {
+        numericLettersWarn.classList.add('text-blue-500');
+    } else {
+        numericLettersWarn.classList.remove('text-blue-500');
+    }
+
+    if (minLetters && capLetter && normalLetter && numericLetter) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+oldPasswordElement.onchange = checkOldPassword;
+newPasswordElement.onkeyup = checkNewPassword;
+
+
+
+
+// Successful notification when updating password
+const successfulNotification = document.createElement('div');
+successfulNotification.setAttribute('id', 'successful-notification');
+successfulNotification.setAttribute('class', 'container mt-28 mx-auto w-80 rounded-lg shadow-xl');
+successfulNotification.innerHTML = `
+    <div class="bg-blue-500 py-4 rounded-lg rounded-b-none">
+        <img src="/imgs/white-tick.png" class="mx-auto w-32">
+    </div>
+    <p class="py-4 mx-14 text-xl font-bold text-center">Thay đổi mật khẩu thành công !</p>
+    <div class="container mx-auto w-1/2 text-center ">
+        <button class="
+            mb-3 px-2 py-1
+            cursor-pointer
+            bg-black rounded-md shadow-md
+            text-white font-semibold
+            focus:outline-none
+            hover:shadow-lg
+            transform hover:scale-110
+            transition duration-75
+            ">
+            Đóng
+        </button>
+    </div>
+`;
+successfulNotification.onclick = (event) => {
+    const currentTarget = event.currentTarget;
+    currentTarget.remove();
+
+    document.body.appendChild(changePasswordWrapper);
+    resetBtn.click();
+}
+
+
+// Successful notification when updating password
+const unsuccessfulNotification = document.createElement('div');
+unsuccessfulNotification.setAttribute('id', 'unsuccessful-notification');
+unsuccessfulNotification.setAttribute('class', 'container mt-28 mx-auto w-80 rounded-lg shadow-xl');
+unsuccessfulNotification.innerHTML = `
+    <div class="bg-gray-200 py-8 rounded-lg rounded-b-none">
+        <img src="/imgs/x-mark.png" class="mx-auto w-24">
+    </div>
+    <p class="py-4 mx-10 text-xl font-bold text-center">Thay đổi mật khẩu không thành công !</p>
+    <div class="container mx-auto w-1/2 text-center ">
+        <button class="
+            mb-3 px-2 py-1
+            cursor-pointer
+            bg-black rounded-md shadow-md
+            text-white font-semibold
+            focus:outline-none
+            hover:shadow-lg
+            transform hover:scale-110
+            transition duration-75
+            ">
+            Đóng
+        </button>
+    </div>
+`;
+unsuccessfulNotification.onclick = (event) => {
+    const currentTarget = event.currentTarget;
+    currentTarget.remove();
+
+    document.body.appendChild(changePasswordWrapper);
+};
+
+
+
+// Reset button for entered data
+let resetBtn = document.querySelector('#reset-btn');
+resetBtn.onclick = () => {
+    oldPasswordElement.value = '';
+    let oldPasswordWarning = document.querySelector('#old-password-warning');
+    if (!oldPasswordWarning.classList.contains('invisible')) {
+        oldPasswordWarning.classList.add('invisible');
+    }
+
+    newPasswordElement.value = '';
+    checkNewPassword();
+}
+
+// Update button
+let updateBtn = document.querySelector('#update-btn');
+updateBtn.onclick = () => {
+    let oldPasswordVal = oldPasswordElement.value.trim();
+    let newPasswordVal = newPasswordElement.value.trim();
+
+    if (checkOldPassword() && checkNewPassword()) {
+        const option = {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                oldPassword: oldPasswordVal,
+                newPassword: newPasswordVal
+            })
+        }
+
+        fetch(`${CONFIG.serverAddress}:${CONFIG.serverPort}/api/user-account`, option)
+            .then(res => res.json())
+            .then(data => {
+                if (data.message == 'success') {
+                    changePasswordWrapper.remove();
+                    document.body.appendChild(successfulNotification);
+                } else {
+                    changePasswordWrapper.remove();
+                    document.body.appendChild(unsuccessfulNotification);
+                }
+            });
+    }
+}
